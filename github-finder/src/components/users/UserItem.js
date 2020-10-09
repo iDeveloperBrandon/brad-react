1ec2a7c30e8369338f2d5f9b9cd422d398b97ed6
iDeveloperBrandon <hsy0602@outlook.com>
2020-10-09 18:11:56 +0900

    13 stateless functional components
    convert from class comp to functional comp

--- github-finder/src/components/users/UserItem.js
+++ github-finder/src/components/users/UserItem.js
@@ -1,27 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-  render() {
-    // passing props, instead of state
-    // because i passed in a prop of user
-    const { avatar_url, login, html_url } = this.props.user;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+  // passing props, instead of state
+  // because i passed in a prop of user
+
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
