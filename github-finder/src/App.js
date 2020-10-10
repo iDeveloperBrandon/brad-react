0e98c40ddf29e2e80808af9a78dc90dbe79ae437
iDeveloperBrandon <hsy0602@outlook.com>
2020-10-10 20:57:23 +0900

    20 alert state & component

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -3,12 +3,14 @@ import axios from "axios";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // original first 30 users
@@ -33,16 +35,25 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  // set alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 2000);
+  };
+
   render() {
     const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
