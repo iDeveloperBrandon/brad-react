ea6691d4751453e3067dca91dfe6408e0938ff4d
iDeveloperBrandon <hsy0602@outlook.com>
2020-11-21 16:34:25 +0900

    Update User.js

--- github-finder/src/components/users/User.js
+++ github-finder/src/components/users/User.js
@@ -1,9 +1,19 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
+
   render() {
     const {
       name,
@@ -21,7 +31,35 @@ export class User extends Component {
 
     const { loading } = this.props;
 
-    return <div>{name}</div>;
+    if (loading) {
+      return <Spinner />;
+    }
+
+    return (
+      <Fragment>
+        <Link to="/" className="btn btn-light">
+          Back to Search
+        </Link>
+        Hireable :{" "}
+        {hireable ? (
+          <i className="fas fa-check text-success " />
+        ) : (
+          <i className="fas fa-times-circle text-danger " />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              alt=""
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
